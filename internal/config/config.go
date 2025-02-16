4ec1a60a8d5f78d4f4a8c0bedefa487aca4d4bff
modestprophet <modestprophet@gmail.com>
2025-02-16 10:37:23 -0600

    service config stuff; install instructions

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,6 +25,16 @@ func Read() (*Config, error) {
 		return nil, fmt.Errorf("failed to get config filepath: %w", err)
 	}
 
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		// Create default config from sample
+		if err := createDefaultConfig(path); err != nil {
+			return nil, fmt.Errorf("failed to create default config: %w", err)
+		}
+		fmt.Printf("Created default config file at: %s\n", path)
+		fmt.Println("Please edit this file with your configuration values")
+		os.Exit(0)
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -48,4 +58,27 @@ func getConfigFilePath() (string, error) {
 		return "", fmt.Errorf("error obtaining user home directory path: %w", err)
 	}
 	return filepath.Join(home, configFileName), nil
+}
+
+func createDefaultConfig(path string) error {
+	defaultConfig := Config{
+		DBURL:        "postgres://dbuser:password@localhost:5432/database?sslmode=disable",
+		MqServerURL:  "tcp://10.0.20.26:1883",
+		MqClientID:   "pirowflo",
+		MqDeviceName: "Waterrower data subscriber",
+		MqUser:       "MQ_USER",
+		MqPassword:   "MQ_PASSWORD",
+		MqTopic:      "waterrower/data",
+	}
+
+	data, err := json.MarshalIndent(defaultConfig, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshaling default config: %w", err)
+	}
+
+	if err := os.WriteFile(path, data, 0600); err != nil {
+		return fmt.Errorf("error writing config file: %w", err)
+	}
+
+	return fmt.Errorf("config not found. please edit the newly created default config created at %s", path)
 }
